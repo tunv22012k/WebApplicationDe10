110d81bbd13f80267d7b1bda3856a2cfebd759b2
TuNV <bp-tuunisoft@unitech.vn>
2024-08-14 22:17:16 +0700

    update code

--- WebApplicationDe10/Controllers/Admin/HomeController.cs
+++ WebApplicationDe10/Controllers/Admin/HomeController.cs
@@ -1,14 +1,24 @@
 ﻿using System.Web.Mvc;
 using WebApplicationDe10.Helpers;
+using WebApplicationDe10.Services;
+using WebApplicationDe10.Services.Admin;
 
 namespace WebApplicationDe10.Controllers.Admin
 {
     [Authorize(Roles = Constants.RoleAdmin)]
     public class HomeController : Controller
     {
+        private readonly HomeService homeService;
+
+        public HomeController()
+        {
+            homeService = new HomeService();
+        }
+
         public ActionResult Index()
         {
             ViewBag.ActivePage = "Index";
+            ViewBag.listUser = homeService.GetAllUsers();
             return View("~/Views/Admin/Home/Index.cshtml");
         }
     }
---
+++ WebApplicationDe10/Services/Admin/HomeService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using WebApplicationDe10.Helpers;
+using WebApplicationDe10.Models;
+
+namespace WebApplicationDe10.Services.Admin
+{
+    public class HomeService
+    {
+        public List<User> GetAllUsers()
+        {
+            string query = "SELECT * FROM users";
+            DataTable dt = DatabaseHelper.ExecuteQuery(query);
+
+            List<User> users = new List<User>();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                User user = new User();
+                user.userId     = (int)row["userId"];
+                user.email      = row["email"].ToString();
+                user.username   = row["username"].ToString();
+                user.role       = row["role"].ToString();
+                users.Add(user);
+            }
+
+            return users;
+        }
+    }
+}
