d148d1c4a031c8e4d429d12e14721b704fc3ec79
TuNV <bp-tuunisoft@unitech.vn>
2024-08-25 22:30:20 +0700

    update code

---
+++ WebApplicationDe10/Controllers/GioiThieuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using WebApplicationDe10.Services;
+
+namespace WebApplicationDe10.Controllers
+{
+    public class GioiThieuController : Controller
+    {
+        public GioiThieuController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.ActivePage = "GioiThieu";
+            return View();
+        }
+    }
+}
